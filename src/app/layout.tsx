853ee4cda3f8b0a4bea1c7bc11e92c969077d9df
dheemanthm04 <dheemanth.cs22@bmsce.ac.in>
2025-07-08 17:21:19 +0530

    vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import { Analytics } from "@vercel/analytics/next"
 const inter = Inter({
   subsets: ["latin"],
   variable: "--font-inter",
@@ -81,6 +81,7 @@ export default function RootLayout({
 
       <body className={`${inter.variable} antialiased`}>
         {children}
+        <Analytics />
       </body>
     </html>
   );
